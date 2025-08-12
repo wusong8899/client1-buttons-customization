a73c3bcc1c9d1d55d09bcbd57c051f09c7606dd4
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-13 08:53:14 +0900

    fix

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = require('flarum-webpack-config')();
+const getFlarumWebpackConfig = require('flarum-webpack-config');
+const config = getFlarumWebpackConfig();
+
+// Exclude node_modules from Babel to avoid mixing ESM imports into CommonJS deps (e.g., mithril).
+const babelRule = (config.module && config.module.rules || []).find(
+  (r) => String(r.loader || '').includes('babel-loader')
+);
+
+if (babelRule) {
+  // Limit transpilation to our source files only.
+  const path = require('path');
+  babelRule.exclude = /node_modules/;
+  babelRule.include = [path.resolve(__dirname, 'src')];
+}
+
+module.exports = config;
